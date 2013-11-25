8f571a09332a1b1fcb9bdd81e9e72d31d6b6c889
Tom Boutell <tom@punkave.com>
2013-11-25 14:13:17 -0500

    Simple test of macro with include bug

--- test.js
+++ test.js
@@ -1,21 +1,5 @@
 var nunjucks = require('nunjucks');
-var env = new nunjucks.Environment();
-var issue61 = env.getTemplate('issue61.html');
-console.log('ISSUE #61: two levels of super should work');
-try {
-  console.log(issue61.render({ }));
-  console.log("Seems to have worked. You should see layout1, layout2 and test mentioned.\n");
-} catch (e) {
-  console.log('Exception thrown:');
-  console.log(e);
-}
-
-var issue62 = env.getTemplate('issue62.html');
-console.log('ISSUE #62: escaping a number should work');
-try {
-  console.log(issue62.render({ number: 5 }));
-  console.log("Seems to have worked. You should see 5.\n");
-} catch (e) {
-  console.log('Exception thrown:');
-  console.log(e);
-}
+nunjucks.configure('.');
+var s = nunjucks.render('macro-with-include.html', { name: 'james' });
+console.log('Should look like: FooInclude james');
+console.log(s);
